76c5060480fb3871efeda2ee507db5c6b905c757
Edwin Medina <edwinnrm@gmail.com>
2023-01-31 21:21:19 -0300

    adiconando funcionalidade de marcado como feito

--- routes/routes.js
+++ routes/routes.js
@@ -6,5 +6,6 @@ routes.post("/create", TaskController.createTask);
 routes.get("/getById/:id/:method", TaskController.getById);
 routes.post("/update/:id", TaskController.updateTask);
 routes.get("/delete/:id", TaskController.deleteTask);
+routes.get("/check/:id", TaskController.checkTask);
 
 module.exports = routes;
